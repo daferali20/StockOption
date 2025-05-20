865cd9451b36843bf02a8e7d81e0f8c4624a500e
daferali20 <31751340+daferali20@users.noreply.github.com>
2025-05-20 18:28:39 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 🟡 هذا السطر مهم لحل مشكلة المسارات
+  base: './',
   build: {
     outDir: 'dist',
   },
 });
-
